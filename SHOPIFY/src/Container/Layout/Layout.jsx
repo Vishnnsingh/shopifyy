b51adbaf073db5fd94d149ad1cd87d585e7d3cf3
vishnusingh <kumarvishnu65834@gmail.com>
2025-02-27 02:47:50 +0530

    updated

--- SHOPIFY/src/Container/Layout/Layout.jsx
+++ SHOPIFY/src/Container/Layout/Layout.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import Style from '../Layout/Layout.module.css'
-import Header from '../../Components/Header/Header'
-import {Outlet} from "react-router-dom"
+import React from "react";
+import { useLocation } from "react-router-dom"; // Import useLocation
+import Style from "../Layout/Layout.module.css";
+import Header from "../../Components/Header/Header";
+import { Outlet } from "react-router-dom";
+
 const Layout = () => {
+  const location = useLocation(); // Get current route
+
   return (
     <div>
-      <Header />
+      {/* Hide Header on Register Page */}
+      {location.pathname !== "/register" && <Header />} 
       <Outlet />
-    
     </div>
-  )
-}
+  );
+};
 
-export default Layout
+export default Layout;
